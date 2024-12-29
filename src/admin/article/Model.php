3656e01631979fce8c37312ec900385e4c42eef4
sunboy <qysk@vip.qq.com>
2024-12-29 18:46:33 +0800

    init

--- src/admin/article/Model.php
+++ src/admin/article/Model.php
@@ -3,9 +3,19 @@
 namespace app\admin\article;
 
 use app\common\controllers\module\admin\ModelAdmin;
+use app\model\article\ArticleField;
 use app\model\article\ArticleModel;
 
 class Model extends ModelAdmin
 {
+    /**
+     * 模型类 模型
+     * @var string
+     */
     protected static string $modelClass = ArticleModel::class;
+    /**
+     * 字段类 模型
+     * @var string
+     */
+    protected static string $fieldClass = ArticleField::class;
 }
