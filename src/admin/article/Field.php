1c7c3273e4b928a51d55289c74906231dd114578
sunboy <qysk@vip.qq.com>
2024-12-29 19:14:29 +0800

    init

--- src/admin/article/Field.php
+++ src/admin/article/Field.php
@@ -2,10 +2,33 @@
 
 namespace app\admin\article;
 
-use app\common\controllers\module\admin\ModelAdmin;
+use app\common\controllers\module\admin\FieldAdmin;
+use app\model\article\ArticleColumn;
+use app\model\article\ArticleContent;
+use app\model\article\ArticleField;
 use app\model\article\ArticleModel;
 
-class Field extends ModelAdmin
+class Field extends FieldAdmin
 {
-    protected static string $modelClass = ArticleModel::class;
+    /**
+     * 字段模型
+     * @var string
+     */
+    protected static string $modelClass = ArticleField::class;
+    /**
+     * 内容设计模型
+     * @var string
+     */
+    protected static string $moduleModelClass = ArticleModel::class;
+
+    /**
+     *内容 模型
+     * @var string
+     */
+    protected static string $contentModelClass = ArticleContent::class;
+    /**
+     * 分类模型
+     * @var string
+     */
+    protected static string $columnModelClass = ArticleColumn::class;
 }
