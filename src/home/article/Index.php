fcf706e8247b0b1972f33283f76441ea457750d1
sunboy <qysk@vip.qq.com>
2025-01-09 10:47:20 +0800

    init

--- src/home/article/Index.php
+++ src/home/article/Index.php
@@ -4,7 +4,6 @@ declare (strict_types=1);
 namespace app\home\article;
 
 use app\common\controllers\BaseHome;
-use mowzs\lib\module\service\ContentBaseService;
 use think\Exception;
 
 class Index extends BaseHome
@@ -16,7 +15,6 @@ class Index extends BaseHome
      */
     public function index(): string
     {
-        dump(ContentBaseService::instance()->getInfo('7'));
         return $this->fetch();
     }
 }
