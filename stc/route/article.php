6c1ed1f6c2fe3247c57823b1394da967d2b16c50
sunboy <qysk@vip.qq.com>
2025-01-15 16:13:54 +0800

    init

--- stc/route/article.php
+++ stc/route/article.php
@@ -4,7 +4,9 @@ use think\facade\Route;
 
 Route::group('article', function () {
     Route::rule(':id', 'article/content/index');
-    Route::rule('list-:id', 'article/column/index');
+    Route::rule('list_:id', 'article/column/index');
+    Route::rule('tag_:id', 'article/tag/show');
+    Route::rule('tag', 'article/tag/index');
     Route::rule('', 'article/index/index');
 })->pattern(['id' => '\d+', 'name' => '\w+']);
 
