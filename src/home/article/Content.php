0803be8a7534659835b2e597bd74bb8acbc59920
sunboy <qysk@vip.qq.com>
2025-03-26 17:08:19 +0800

    refactor(service): 重构服务层逻辑
    - 将所有 Service 类更名为 Logic 类，统一命名规范- 更新所有相关引用，以适应新的类名
    - 此次重构不影响功能，仅更改了类的命名和组织结构

--- src/home/article/Content.php
+++ src/home/article/Content.php
@@ -3,13 +3,8 @@
 namespace app\home\article;
 
 use mowzs\lib\module\controller\home\ContentHome;
-use mowzs\lib\module\service\ContentBaseService;
 
 class Content extends ContentHome
 {
-    /**
-     * 定义服务类
-     * @var string
-     */
-    protected static string $serviceClass = ContentBaseService::class;
+
 }
