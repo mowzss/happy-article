c42a8b0f16909d37837fa7ce7c92aaee63fca808
sunboy <qysk@vip.qq.com>
2025-03-26 16:20:05 +0800

    refactor(tag): 移除标签页面路由并优化标签相关功能- 删除了多个模块中的 tag 页面路由 - 添加了 ArticleTag 中的 getAjaxList 方法 -优化了 TagAdmin 类中的 getAjaxList 方法 - 修正了 TagBaseModel 中的 suffix 方法调用

--- stc/route/article.php
+++ stc/route/article.php
@@ -6,6 +6,5 @@ Route::group('article', function () {
     Route::rule(':id', 'article/content/index');
     Route::rule('list_:id', 'article/column/index');
     Route::rule('tag_:id', 'article/tag/show');
-    Route::rule('tag', 'article/tag/index');
     Route::rule('', 'article/index/index');
 })->pattern(['id' => '\d+', 'name' => '\w+']);
