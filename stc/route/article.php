42186bf3392e0672f6c3379f99e09f08c9e3e365
sunboy <qysk@vip.qq.com>
2025-07-28 23:39:10 +0800

    refactor(routing): 优化多个模块的路由配置
    - 更新了多个模块的路由规则，将 controller 和 action 之间的斜杠替换为点
    - 在多个路由配置中添加了 ->auto() 方法，启用自动路由
    - 移除了不必要的 pattern 配置项
    -优化了路由配置的结构和可读性

--- stc/route/article.php
+++ stc/route/article.php
@@ -3,8 +3,8 @@
 use think\facade\Route;
 
 Route::group('article', function () {
-    Route::rule(':id', 'article/content/index');
-    Route::rule('list_:id', 'article/column/index');
-    Route::rule('tag_:id', 'article/tag/show');
-    Route::rule('', 'article/index/index');
-})->pattern(['id' => '\d+', 'name' => '\w+']);
+    Route::rule(':id', 'article.content/index');
+    Route::rule('list_:id', 'article.column/index');
+    Route::rule('tag_:id', 'article.tag/show');
+    Route::rule('', 'article.index/index');
+})->pattern(['id' => '\d+'])->auto();
