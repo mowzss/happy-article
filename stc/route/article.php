f9170fdc8fe28e417abd4be72f8aa0908b4eb4ca
sunboy <qysk@vip.qq.com>
2025-07-29 09:49:39 +0800

    refactor(routing): 更新多个模块的路由配置并添加自动路由- 在多个模块中更新了路由规则，增加了模块前缀以避免潜在的命名冲突 - 在每个模块的路由配置末尾添加了 Route::auto() 以启用自动路由功能 - 优化了部分路由规则的定义，提高了代码的可读性和可维护性

--- stc/route/article.php
+++ stc/route/article.php
@@ -3,8 +3,9 @@
 use think\facade\Route;
 
 Route::group('article', function () {
-    Route::rule(':id', 'content/index');
-    Route::rule('list_:id', 'column/index');
-    Route::rule('tag_:id', 'tag/show');
-    Route::rule('', 'index/index');
+    Route::rule(':id', 'article/content/index');
+    Route::rule('list_:id', 'article/column/index');
+    Route::rule('tag_:id', 'article/tag/show');
+    Route::rule('', 'article/index/index');
 })->pattern(['id' => '\d+']);
+Route::auto();
