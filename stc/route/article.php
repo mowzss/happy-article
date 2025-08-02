06370d9511e93768b4c5872cfc62da279004b8fb
sunboy <qysk@vip.qq.com>
2025-08-02 10:38:59 +0800

    refactor:修正路由规则 兼容thinkphp8.1.3

--- stc/route/article.php
+++ stc/route/article.php
@@ -2,7 +2,13 @@
 
 use think\facade\Route;
 
+//前台内容页
 Route::rule('article/:id', 'article/content/index')->pattern(['id' => '\d+']);
+//后台内容管理
+Route::rule('article/content_index', 'article/content/index');
+//前台栏目内容列表
 Route::rule('article/list_:id', 'article/column/index')->pattern(['id' => '\d+']);
+//后台栏目管理
+Route::rule('article/column_index', 'article/column/index')->pattern(['id' => '\d+']);
+//前台标签内容列表
 Route::rule('article/tag_:id', 'article/tag/show')->pattern(['id' => '\d+']);
-Route::rule('article', 'article/index/index')->pattern(['id' => '\d+']);
