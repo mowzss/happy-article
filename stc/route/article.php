7fce9f7d0df91b0a8eb75bffc7952971054069fe
sunboy <qysk@vip.qq.com>
2026-04-10 11:07:15 +0800

    style(route): 调整路由文件中的注释缩进格式
    - 统一了后台内容管理注释的缩进格式
    - 统一了前台栏目内容列表注释的缩进格式
    - 统一了后台栏目管理注释的缩进格式
    - 统一了前台标签内容列表注释的缩进格式
    - 保持了原有的路由规则不变

--- stc/route/article.php
+++ stc/route/article.php
@@ -6,12 +6,12 @@ Route::group('article', static function () {
     Route::rule('', 'article/index/index');
     //前台内容页
     Route::rule(':id', 'article/content/index');
-//后台内容管理
+    //后台内容管理
     Route::rule('content_index', 'article/content/index');
-//前台栏目内容列表
+    //前台栏目内容列表
     Route::rule('list_:id', 'article/column/index');
-//后台栏目管理
+    //后台栏目管理
     Route::rule('column_index', 'article/column/index');
-//前台标签内容列表
+    //前台标签内容列表
     Route::rule('tag_:id', 'article/tag/show');
 })->pattern(['id' => '\d+']);
